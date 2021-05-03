d21071e4da10a832fdf88a2d3dfa742fa05f6ec7
jeremy <htcp3450@mail.ru>
2021-05-03 17:18:04 +0300

    Added main page, operators page, security config. closes #3

---
+++ src/main/java/com/balakin/qa/services/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.balakin.qa.services;
+
+import com.balakin.qa.domain.User;
+import com.balakin.qa.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+        User user = userRepository.findByLogin(userName)
+                .orElseThrow(() -> new UsernameNotFoundException("Login " + userName + " not found"));
+        System.out.println(user.getLogin()+" / "+user.getPassword());
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(),
+                getAuthorities(user));
+    }
+
+    private static Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        String[] userRoles = user.getRoles().stream().map((role) -> role.getName()).toArray(String[]::new);
+        Collection<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(userRoles);
+        return authorities;
+    }
+}
